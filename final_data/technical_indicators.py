fffecdaaf1ffe04abb8dddfdd04f98c2349954b1
s-h-a-d-o-w-C-l-o-n-e <singhrohan2533@gmail.com>
2023-03-02 00:17:28 +0530

    n

--- final_data/technical_indicators.py
+++ final_data/technical_indicators.py
@@ -1,89 +1,159 @@
+# CCI
+# 
+
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
 from ta.volume import VolumeWeightedAveragePrice
 
+# Price change with %-------------------------------
+def _price_change(df):
+    df['price_change'] = df['close'] - df['close'].shift(1)
+    df['price_pct_change'] = df['price_change'] / df['close'].shift(1) * 100
+    return df
+
+
+# EMA--------------------------------------------
+
 def calculate_ema(data, n):
     ema = data.close.ewm(span=n, adjust=False).mean()
     return ema
 
-def generate_signals_vwap(df):
-    sell_signal = df['close'] < (df['vwap'] + (0.001 * df['close']))
-    buy_signal = df['close'] > (df['vwap'] + (0.001 * df['close']))
-    return buy_signal, sell_signal
-
-def generate_signals_rsi(df):
-    buy_signal = (df['rsi'] < 30) & (df['rsi'].shift(1) >= 30)
-    sell_signal = (df['rsi'] > 70) & (df['rsi'].shift(1) <= 70)
-    return buy_signal, sell_signal
-
-def generate_signals_stochrsi(df):
-    overbought = 80
-    oversold = 20
-
-    # Create boolean masks for overbought and oversold levels
-    is_overbought = df['stochrsi'] > overbought
-    is_oversold = df['stochrsi'] < oversold
-    return is_overbought, is_oversold
-#
-#
-#
-
-# EMA
 def _ema_n(df, n):
     close_price = df['close']
     ema_n = calculate_ema(df, n)
-    ema_n = ema_n.round(3)
+    ema_n = ema_n.round(6)
     df['ema_{}'.format(n)] = ema_n
     return df
 
-# VWAP
+def _ema_signal(df):
+    diff100 = (df['close'] - df['ema_100'])/df['ema_100']
+    diff200 = (df['close'] - df['ema_200'])/df['ema_200']
+    # Create the signal column
+    signal = pd.Series(0, index=df.index)
+    signal[(diff100 > 0.0025) & (diff200 > 0.002)] = 1
+    signal[(diff100 < -0.0025) & (diff200 < -0.002)] = -1
+
+    df['ema_signal'] = signal
+    
+    return df
+
+
+# VWAP-----------------------------------------------
+
+# def generate_signals_vwap(df):
+#     sell_signal = df['close'] < (df['vwap'] + (0.001 * df['close']))
+#     buy_signal = df['close'] > (df['vwap'] + (0.001 * df['close']))
+#     return buy_signal, sell_signal
+
+# def _vwap(df, label='vwap', window=3, fillna=True):
+#         vwap_hcl3 = VolumeWeightedAveragePrice(high=df['high'], low=df['low'], close=df["close"], volume=df['volume'], window=window, fillna=fillna).volume_weighted_average_price()
+#         df[label] = vwap_hcl3
+#         buy_signal, sell_signal = generate_signals_vwap(df)
+
+#         # Add signals to the DataFrame
+#         df['vwap_buy_signal'] = buy_signal.astype(int)
+#         df['vwap_sell_signal'] = sell_signal.astype(int)
+
+#         return df
+
+def generate_signal_vwap(df):
+    signal = pd.Series(data=np.zeros(len(df)), index=df.index)
+    signal[df['close'] > (df['vwap'] + (0.001 * df['close']))] = 1
+    signal[df['close'] < (df['vwap'] + (0.001 * df['close']))] = -1
+    return signal.astype(int)
+
 def _vwap(df, label='vwap', window=3, fillna=True):
-        vwap_hcl3 = VolumeWeightedAveragePrice(high=df['high'], low=df['low'], close=df["close"], volume=df['volume'], window=window, fillna=fillna).volume_weighted_average_price()
-        df[label] = vwap_hcl3
-        buy_signal, sell_signal = generate_signals_vwap(df)
-
-        # Add signals to the DataFrame
-        df['vwap_buy_signal'] = buy_signal.astype(int)
-        df['vwap_sell_signal'] = sell_signal.astype(int)
-
-        return df
-
-# RSI 
-def _rsi(df):
-    rsi = ta.momentum.rsi(df['close'], window=14) # to add length add , length=6 default 14
-    df['rsi'] = rsi
-    buy_signal, sell_signal = generate_signals_rsi(df)
-
-    # Add signals to the DataFrame
-    df['rsi_buy_signal'] = buy_signal.astype(int)
-    df['rsi_sell_signal'] = sell_signal.astype(int)
+    vwap_hcl3 = VolumeWeightedAveragePrice(high=df['high'], low=df['low'], close=df['close'], volume=df['volume'], window=window, fillna=fillna).volume_weighted_average_price()
+    df[label] = vwap_hcl3
+    signal = generate_signal_vwap(df)
+    df['vwap_signal'] = signal
     return df
 
-# stochastic RSI 
-def _stochrsi(df):
-    stochrsi = ta.stochrsi(df['close'])
-    df['stochrsi'] = stochrsi
-    is_overbought, is_oversold = generate_signals_stochrsi(df)
+# RSI-------------------------------------------
 
-    # Add overbought and oversold signals to the DataFrame
-    df['stochrsi_sell_signal'] = is_overbought.astype(int)
-    df['stochrsi_buy_signal'] = is_oversold.astype(int)
+# def generate_signals_rsi(df):
+#     buy_signal = (df['rsi'] < 30) & (df['rsi'].shift(1) >= 30)
+#     sell_signal = (df['rsi'] > 70) & (df['rsi'].shift(1) <= 70)
+#     return buy_signal, sell_signal
+
+# def _rsi(df):
+#     rsi = ta.momentum.rsi(df['close'], window=14) # to add length add , length=6 default 14
+#     df['rsi'] = rsi
+#     buy_signal, sell_signal = generate_signals_rsi(df)
+
+#     # Add signals to the DataFrame
+#     df['rsi_buy_signal'] = buy_signal.astype(int)
+#     df['rsi_sell_signal'] = sell_signal.astype(int)
+#     return df
+
+
+# stochastic RSI--------------------------------------
+
+# def _stochrsi(df, overbought=80, oversold=20):
+#     # Calculate Stochastic RSI
+#     stochrsi = df.ta.stoch(high='high', low='low', close='close', k=14, d=3, append=True)
+
+#     # Create boolean masks for overbought and oversold levels
+#     is_overbought = (stochrsi['STOCHk_14_3_3'] > overbought) & (stochrsi['STOCHd_14_3_3'] > overbought) & (stochrsi['STOCHk_14_3_3'] < stochrsi['STOCHd_14_3_3'])
+#     is_oversold = (stochrsi['STOCHk_14_3_3'] < oversold) & (stochrsi['STOCHd_14_3_3'] < oversold) & (stochrsi['STOCHk_14_3_3'] > stochrsi['STOCHd_14_3_3'])
+
+#     # Add signals to the DataFrame
+#     df['stochrsi_sell_signal'] = is_overbought.astype(int)
+#     df['stochrsi_buy_signal'] = is_oversold.astype(int)
+
+#     return df
+
+def _stochrsi(df, overbought=80, oversold=20):
+    # Calculate Stochastic RSI
+    stochrsi = df.ta.stoch(high='high', low='low', close='close', k=14, d=3, append=True)
+
+    # Create boolean masks for overbought and oversold levels
+    is_overbought = (stochrsi['STOCHk_14_3_3'] > overbought) & (stochrsi['STOCHd_14_3_3'] > overbought) & (stochrsi['STOCHk_14_3_3'] < stochrsi['STOCHd_14_3_3'])
+    is_oversold = (stochrsi['STOCHk_14_3_3'] < oversold) & (stochrsi['STOCHd_14_3_3'] < oversold) & (stochrsi['STOCHk_14_3_3'] > stochrsi['STOCHd_14_3_3'])
+
+    # Reindex the boolean masks to align them
+    is_overbought = is_overbought.reindex(df.index, fill_value=False)
+    is_oversold = is_oversold.reindex(df.index, fill_value=False)
+
+    # Create the signal column
+    signal = pd.Series(np.zeros(len(df)), index=df.index)
+    signal[is_overbought] = -1
+    signal[is_oversold] = 1
+
+    # Add the signal column to the DataFrame
+    df['stochrsi_signal'] = signal.astype(int)
     return df
 
-# MACD 
+# MACD -------------------------------------------------
+
+# def _macd(df):
+#     macd = ta.macd(df['close'])
+#     df['MACD_12_26_9'] = macd['MACD_12_26_9']
+#     # Create a column for MACD histogram
+#     macd_histogram = macd['MACDh_12_26_9']
+
+#     # Identify buy and sell signals
+#     buy_signal = macd['MACD_12_26_9'] > macd['MACDs_12_26_9']
+#     sell_signal = macd['MACD_12_26_9'] < macd['MACDs_12_26_9']
+
+#     # Add buy and sell signals to the DataFrame
+#     df['macd_buy_signal'] = buy_signal.astype(int)
+#     df['macd_sell_signal'] = sell_signal.astype(int)
+
+#     return df
+
 def _macd(df):
     macd = ta.macd(df['close'])
+    df['MACD_12_26_9'] = macd['MACD_12_26_9']
+    df['MACDs_12_26_9'] = macd['MACDs_12_26_9']
+    df['MACDh_12_26_9'] = macd['MACDh_12_26_9']
+    # Create a signal column based on buy and sell signals
+    signal = 0
+    signal[macd['MACD_12_26_9'] > macd['MACDs_12_26_9']] = 1
+    signal[macd['MACD_12_26_9'] < macd['MACDs_12_26_9']] = -1
 
-    # Create a column for MACD histogram
-    macd_histogram = macd['MACDh']
-
-    # Identify buy and sell signals
-    buy_signal = macd['MACD'] > macd['SIGNAL']
-    sell_signal = macd['MACD'] < macd['SIGNAL']
-
-    # Add buy and sell signals to the DataFrame
-    df['macd_buy_signal'] = buy_signal.astype(int)
-    df['macd_sell_signal'] = sell_signal.astype(int)
+    # Add signal column to the DataFrame
+    df['macd_signal'] = signal
 
     return df
